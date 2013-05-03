ca83edb2fbe3d79bb92495d35f9a64a2c4ed3c4e
Hasan Kumar Reddy A <mintuhouse@gmail.com>
2013-05-04 01:32:56 +0530

    Modified classpath

---
+++ PostgreSQL_index_tuning/src/in/ac/iitb/cse/dbms/pg_indextuning/IndexTuner.java
@@ -0,0 +1,8 @@
+package in.ac.iitb.cse.dbms.pg_indextuning;
+
+public class IndexTuner {                                                                   
+	
+	public IndexTuner(DBConnection con){
+		
+	}
+}
